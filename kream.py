e9dc33040c8042dd768704d2dd5eca412e73dc30
badatga <widdefinesme@gmail.com>
2025-08-07 14:22:11 +0900

    kream scraping & db connect

--- kream.py
+++ kream.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 # 키보드 입력
 from selenium.webdriver.common.keys import Keys
 
-
+import mysql.connector
 import time
 
 keyword = input("검색어 입력 : ")
@@ -34,14 +34,33 @@ html = driver.page_source
 soup = BeautifulSoup(html, "html.parser")
 
 items = soup.select(".item_inner")
+product_list = []
 
 for item in items:
   product_name = item.select_one(".translated_name").text
 
   if '후드' in product_name:
-    print(f"제품명 : {product_name}")
+    category = "상의"
     brand = item.select_one('.brand-name').text
     price = item.select_one('.amount').text
+    product_info = [category, brand, product_name, price]
+    product_list.append(product_info)
+
+connection = mysql.connector.connect(
+  host = '127.0.0.1',
+  user = 'root',
+  passwd = '',
+  db = 'KREAM',
+  charset = 'utf8mb4'
+)
+
+def execute_query(connetion, query, args=None):
+  with connection.cursor() as cursor:
+    cursor.execute(query, args or ())
+    if query.strip().upper().startswith('SELECT'):
+      return cursor.fetchall()
+    else:
+      connection.commit()
 
 driver.quit()
 
